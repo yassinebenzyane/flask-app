bca88788f6474696554dce6c8add1d3abaa027b9
Yassine <y.benzyane@edu.umi.ac.ma>
2026-03-09 14:03:18 +0000

    Add comment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py - A simple Flask application to demonstrate a Docker CI pipeline for DevOps.
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +9,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
